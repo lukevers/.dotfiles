4e191aae9f348ddc4e2464670cfa23c4744dbc6f
Luke Evers <me@lukevers.com>
2014-06-06 19:45:07 -0400

    Added tmux

--- link.sh
+++ link.sh
@@ -25,6 +25,18 @@ then
 	ln -s $DOTFILES/vim/vim/ $HOME/.vim
 fi
 
+# Tmux Colors
+if [[ ! -L "$HOME/.tmux.colors" ]]
+then
+	ln -s $DOTFILES/tmux/tmux.colors $HOME/.tmux.colors
+fi
+
+# Tmux Config
+if [[ ! -L "$HOME/.tmux.conf" ]]
+then
+	ln -s $DOTFILES/tmux/tmux.conf $HOME/.tmux.conf
+fi
+
 ####################
 # Export Variables #
 ####################
