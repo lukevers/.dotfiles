13d1cb48dfccde2f228b45a3f0247ec3774162a9
Luke Evers <me@lukevers.com>
2014-06-06 15:38:30 -0400

    Added vim to dotfiles repo

--- link.sh
+++ link.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
-ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
+HOME=/Users/lukevers
+REPO=$HOME/.dotfiles
+
+# Git
+ln -fs $REPO/git/gitconfig $HOME/.gitconfig
+
+# Vim
+ln -fs $REPO/vim/vimrc $HOME/.vimrc
+ln -fs $REPO/vim/vim/ $HOME/.vim
 
